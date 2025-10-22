54a3ddd2170d2222c4af6d5587f70aabe6f8973c
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-10-22 14:13:19 +0700

    detailing

--- Database & ORM/gorm.go
+++ Database & ORM/gorm.go
@@ -7,14 +7,14 @@ import "gorm.io/gorm"
 type User struct {
 	Name	string
 	Email	string	`gorm:""unique`
-	Tasks	[]Task
-	gorm.Model
+	Tasks	[]Task	// Relasi one to many: satu user bisa memiliki banyak task
+	gorm.Model		// Otomatis menambahkan ID, CreatedAt, UpdateAt, DeleteAt
 }
 
 // struct Task akan menajadi table tasks
 type Task struct {
 	Title	string
 	IsDone	bool
-	UserID	uint
+	UserID	uint	// Ini adalah foreign key yang menghubungkan ke User
 	gorm.Model
 }
